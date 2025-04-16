bf9afe93ca36d1cc13626ee5d8701800cc080aad
Harsh Khandelwal <harsh.khandelwal@hyperface.co>
2025-04-17 00:19:21 +0530

    feat/websocket-connection

---
+++ app.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI
+from fastapi.templating import Jinja2Templates
+from fastapi.requests import Request
+from fastapi.websockets import WebSocket
+from manager import WebSocketManager
+
+
+app = FastAPI()
+
+templates = Jinja2Templates(
+    directory="templates"
+)
+
+manager = WebSocketManager()
+
+@app.get('/')
+async def root(request : Request):
+    return templates.TemplateResponse(
+        request,
+        'index.html',
+        {}
+    )
+    
+@app.websocket("/ws")
+async def websocket_endpoint(websocket : WebSocket):
+    await manager.connect(websocket)
+    
